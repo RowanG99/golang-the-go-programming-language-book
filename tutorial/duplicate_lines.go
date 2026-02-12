e41ee95836a37f25a481a1edbd3948a6d2e180c6
Rowan Gauld <rowangauld1@gmail.com>
2026-02-13 00:22:57 +0200

    opening file, reading inputs using scanner

--- tutorial/duplicate_lines.go
+++ tutorial/duplicate_lines.go
@@ -6,16 +6,48 @@ import (
     "os"
 )
 
-func main(){
-   counts := make(map[string]int)
-   input := bufio.NewScanner(os.Stdin)
-   for input.Scan(){
-       counts[input.Text()]++
-   }
+// func main(){
+//    counts := make(map[string]int)
+//    input := bufio.NewScanner(os.Stdin)
+//    for input.Scan(){
+//        counts[input.Text()]++
+//    }
+// 
+//    for line, n := range counts {
+//        if n > 1 {
+// 	       fmt.Printf("%d\t%s\n", n, line)
+//        }
+//    }
+// }
 
-   for line, n := range counts {
-       if n > 1 {
-	       fmt.Printf("%d\t%s\n", n, line)
-       }
-   }
+func main(){
+    counts := make(map[string]int)
+    files := os.Args[1:]
+
+    if len(files) == 0 {
+	count_lines(os.Stdin, counts)
+    } else {
+	for _, arg := range files {
+	     f, err := os.Open(arg)
+	     if err != nil {
+		 fmt.Fprintf(os.Stderr, "dup2: %\n", err)
+		 continue
+	     }
+	     count_lines(f, counts)
+	     f.Close()
+	}
+    }
+
+    for line, n := range counts {
+	if n > 1 {
+	    fmt.Printf("%d\t%s\n", n, line)
+	}
+    }
+}
+
+func count_lines(f *os.File, counts map[string]int){
+    input := bufio.NewScanner(f)
+    for input.Scan(){
+        counts[input.Text()]++
+    }
 }
