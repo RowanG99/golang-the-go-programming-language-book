97a263b00e41e0bfe3f138eba846cbe5740b9093
Rowan Gauld <rowangauld1@gmail.com>
2026-02-18 00:21:14 +0200

    exe 1.7

--- tutorial/fetching_urls.go
+++ tutorial/fetching_urls.go
@@ -2,27 +2,45 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
+    "io"
     "net/http"
     "os"
 )
 
 func main(){
-   for _, url := range os.Args[1:]{
-       resp, err := http.Get(url)
-       if err != nil {
-	   fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-	   os.Exit(1)
-       }
+    for _, url := range os.Args[1:]{
+	resp, err := http.Get(url)
 
-       b, err := ioutil.ReadAll(resp.Body)
-       resp.Body.Close()
+	if err != nil {
+	 fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+	}
 
-       if err != nil {
-	   fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-	   os.Exit(1)
-       }
+	dst := os.Stdout
 
-       fmt.Printf("%s", b)
-   }
+	bytes, err := io.Copy(dst, resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+	 fmt.Fprintf(os.Stderr, "Copy: %v\n", err)
+	}
+
+	fmt.Printf("%s", bytes)	
+    }
+//    for _, url := range os.Args[1:]{
+//        resp, err := http.Get(url)
+//        if err != nil {
+// 	   fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+// 	   os.Exit(1)
+//        }
+// 
+//        b, err := ioutil.ReadAll(resp.Body)
+//        resp.Body.Close()
+// 
+//        if err != nil {
+// 	   fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+// 	   os.Exit(1)
+//        }
+// 
+//        fmt.Printf("%s", b)
+//    }
 }
